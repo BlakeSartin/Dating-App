a0d2f8450209f8d0ecebf3c375049ec5b0980467
Thomas Ngo <thomasngo818@gmail.com>
2022-03-20 15:41:03 -0700

    Replace use of old db module with knex

--- api/app.js
+++ api/app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter(db));
+app.use("/users", usersRouter(knex));
 app.use("/testAPI", testAPIRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
