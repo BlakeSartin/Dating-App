111d4ef0e713712071b6692e81da67261bdf8eda
Thomas Ngo <thomasngo818@gmail.com>
2022-03-20 15:53:21 -0700

    Comment out unused module for removal

--- api/db/index.js
+++ api/db/index.js
@@ -1,12 +1,12 @@
-const pg = require("pg");
+// const pg = require("pg");
 
-const client = new pg.Client({
-  connectionString: process.env.DATABASE_URL || "",
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
-});
+// const client = new pg.Client({
+//   connectionString: process.env.DATABASE_URL || "",
+//   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
+// });
 
-client
-  .connect()
-  .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
+// client
+//   .connect()
+//   .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
-module.exports = client;
+// module.exports = client;
