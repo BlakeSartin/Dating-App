a72c6371344da5f899d33df7b2a03ada5441de87
Thomas Ngo <thomasngo818@gmail.com>
2022-03-19 19:02:15 -0700

    Add test database call

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+// /* GET users listing. */
+// router.get("/", function (req, res, next) {
+//   res.send("respond with a resource");
+// });
 
-module.exports = router;
+// module.exports = router;
+
+module.exports = (db) => {
+  router.get("/", (request, response) => {
+    db.query(
+      `
+      SELECT * FROM test;
+      `
+    ).then((data) => {
+      console.log(data);
+      response.send(data.rows);
+    });
+  });
+
+  return router;
+};
