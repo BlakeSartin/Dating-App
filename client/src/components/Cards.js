46d7756711763475500a0e9ba27c6743a495d169
Blake Sartin <blake.sartin8@gmail.com>
2022-03-21 15:46:10 -0700

    add more advance functions to card component, along with buttons

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,37 +1,107 @@
-import React, { useState } from 'react'
-import TinderCard from 'react-tinder-card'
-import "./cards.scss"
-function Cards(){
-  const [people, setPeople] = useState([
-    {
-      name: "prince",
-      url:"https://www.chicagotribune.com/resizer/3U1sOcVhiya2oB1GGaSO2GfYD8A=/415x508/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/NKTE7ZA6RJAAPNMPH4XN2IRRTA.jpg"
-    },
-    {
-      name: 'fredie mercury',
-      url: "https://cdns-images.dzcdn.net/images/artist/d690b6b1e9ff8f0a7944b2293b540966/500x500.jpg"
+import React, { useState, useRef, useMemo, useEffect } from "react";
+import TinderCard from "react-tinder-card";
+import "./cards.scss";
+const db = [
+  {
+    name: "prince",
+    url: "https://www.chicagotribune.com/resizer/3U1sOcVhiya2oB1GGaSO2GfYD8A=/415x508/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/NKTE7ZA6RJAAPNMPH4XN2IRRTA.jpg",
+  },
+  {
+    name: "fredie mercury",
+    url: "https://cdns-images.dzcdn.net/images/artist/d690b6b1e9ff8f0a7944b2293b540966/500x500.jpg",
+  },
+];
+
+function Cards() {
+  const [people, setPeople] = useState(db.length - 1);
+
+  const currentPeopleRef = useRef(people);
+
+  const childRefs = useMemo(
+    () =>
+      Array(db.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const updatePeople = (val) => {
+    setPeople(val);
+    currentPeopleRef.current = val;
+  };
+
+  const canGoBack = people < db.length - 1;
+
+  const canSwipe = people >= 0;
+
+  const outOfFrame = (name, idx) => {
+    console.log(`${name} (${idx}) left the screen!`, currentPeopleRef.current)
+    // handle the case in which go back is pressed before card goes outOfFrame
+    currentPeopleRef.current >= idx && childRefs[idx].current.restoreCard()
+    // TODO: when quickly swipe and restore multiple times the same card,
+    // it happens multiple outOfFrame events are queued and the card disappear
+    // during latest swipes. Only the last outOfFrame event should be considered valid
+  }
+
+  const swipe = async (dir) => {
+    if (canSwipe && people < db.length) {
+      await childRefs[people].current.swipe(dir); // Swipe the card!
     }
-  ])
+  };
 
-  
+  const swiped = (direction, nameToDelete, index) => {
+    updatePeople(index - 1);
+  };
 
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newPeople = people + 1;
+    updatePeople(newPeople);
+    await childRefs[newPeople].current.restoreCard();
+  };
+
+  const input = useRef();
+  console.log(input);
   return (
     <div>
       <h1>Cards</h1>
 
-      {people.map(person => (
+      {db.map((person, index) => (
         <TinderCard
-        className= "swipe"
-        key={(person.name)}
-        preventSwipe={['up', 'down']}
+          ref={childRefs[index]}
+          className="swipe"
+          key={person.name}
+          onSwipe={(dir) => swiped(dir, person.name, index)}
+          onCardLeftScreen={() => outOfFrame(person.name, index)}
+          preventSwipe={["up", "down"]}
         >
-          <div style={{ backgroundImage: `url(${person.url})`}} className='card'>
+          <div
+            style={{ backgroundImage: `url(${person.url})` }}
+            className="card"
+          >
             <h3>{person.name}</h3>
-              </div>
+          </div>
         </TinderCard>
       ))}
+      <div className="buttons">
+        <button
+          onClick={() => swipe("left")}
+        >
+          Swipe left!
+        </button>
+        <button
+          onClick={() => goBack()}
+        >
+          Undo swipe!
+        </button>
+        <button
+          onClick={() => swipe("right")}
+        >
+          Swipe right!
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Cards;
