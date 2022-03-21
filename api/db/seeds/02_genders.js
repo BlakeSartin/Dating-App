af5d0de3372922f607bbe58437d747ca8a9c8778
Thomas Ngo <thomasngo818@gmail.com>
2022-03-21 13:57:26 -0700

    Add hardcoded array of gender names to seed file

--- api/db/seeds/02_genders.js
+++ api/db/seeds/02_genders.js
@@ -1,13 +1,42 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+
+//  array of gender names
+genderNames = [
+  "Woman",
+  "Man",
+  "Agender",
+  "Androgynous",
+  "Bigender",
+  "Cis Man",
+  "Cis Woman",
+  "Genderfluid",
+  "Genderqueer",
+  "Gender Nonconforming",
+  "Hijra",
+  "Intersex",
+  "Non-binary",
+  "Other gender",
+  "Pangender",
+  "Transfeminine",
+  "Transgender",
+  "Trans Man",
+  "Transmasculine",
+  "Transsexual",
+  "Trans Woman",
+  "Two Spirit",
+];
+
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex("genders").del();
+
+  // const genders = [];
+  // for (const gender of genderNames) {
+  //   genders.push({ name: gender });
+  // }
+
+  await knex("genders").insert(genders);
 };
