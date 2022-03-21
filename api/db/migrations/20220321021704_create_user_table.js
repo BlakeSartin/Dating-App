16f37b9ed6e756619fdc78821ecef8c624c7494e
Thomas Ngo <thomasngo818@gmail.com>
2022-03-20 19:33:31 -0700

    Add users table migration

---
+++ api/db/migrations/20220321021704_create_user_table.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("id");
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("email").notNullable();
+    table.string("summary");
+    table.text("profile");
+    table.timestamp("last_seen").defaultTo(knex.fn.now());
+    table.timestamps([useTimestamps], [defaultToNow]);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
