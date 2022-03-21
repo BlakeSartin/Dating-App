49ba2422b3f97021e3523cabf5917a2ac46d92bb
Thomas Ngo <thomasngo818@gmail.com>
2022-03-21 14:44:45 -0700

    Add implementation of up fucntion to create user gender identity table and it's relations

--- api/db/migrations/20220321212953_create_gender_idenity-table.js
+++ api/db/migrations/20220321212953_create_gender_idenity-table.js
@@ -2,12 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("user_gender_identity", function (table) {
+    table.increments("id");
+    table.integer("user_id").references("id").inTable("users");
+    table.integer("gender_id").references("id").inTable("genders");
+  });
+};
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable("gender_identity");
+  return knex.schema.dropTable("user_gender_identity");
 };
