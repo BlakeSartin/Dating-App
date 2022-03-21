2a4152ffab91f160fcdae777020bcd8563670c73
Thomas Ngo <thomasngo818@gmail.com>
2022-03-21 10:23:55 -0700

    Add helper function to create fake user object

--- api/db/seeds/01_users.js
+++ api/db/seeds/01_users.js
@@ -1,8 +1,20 @@
+// middleware for creating fake data
 const faker = require("@faker-js/faker");
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
+
+// Helper function to generate fake user
+const createFakeUser = () => ({
+  first_name: faker.name.firstName(),
+  last_name: faker.name.lastName(),
+  email: faker.internet.email(),
+  summary: faker.lorem.lines(),
+  text: faker.lorem.paragraphs(),
+});
+
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("users").del();
