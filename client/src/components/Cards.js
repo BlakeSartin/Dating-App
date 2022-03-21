fab1a580f3cc290694bf04df5d45b43b9a0b3ef2
Blake Sartin <blake.sartin8@gmail.com>
2022-03-21 13:09:16 -0700

    card styling

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import TinderCard from 'react-tinder-card'
-import {withRouter} from "react-router-dom"
+import "./cards.scss"
 function Cards(){
   const [people, setPeople] = useState([
     {
