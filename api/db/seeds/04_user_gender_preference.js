7b87116a938d1cb7c841ae7d96181d0016bf7a91
Thomas Ngo <thomasngo818@gmail.com>
2022-03-21 19:23:32 -0700

    Implement data generation of user gender preferences for inserting into database

--- api/db/seeds/04_user_gender_preference.js
+++ api/db/seeds/04_user_gender_preference.js
@@ -1,13 +1,36 @@
+import { generateGenderId } from "../helpers";
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex("user_gender_preference").del();
+
+  const userPreferences = [];
+  // create 5 gender identities for each user
+  for (let i = 1; i <= fakeUsersCount; i++) {
+    userPreferences.push({
+      user_id: i,
+      gender_id: generateGenderId(),
+    });
+    userPreferences.push({
+      user_id: i,
+      gender_id: generateGenderId(),
+    });
+    userPreferences.push({
+      user_id: i,
+      gender_id: generateGenderId(),
+    });
+    userPreferences.push({
+      user_id: i,
+      gender_id: generateGenderId(),
+    });
+    userPreferences.push({
+      user_id: i,
+      gender_id: generateGenderId(),
+    });
+  }
+
+  await knex("user_gender_preference").insert(userPreferences);
 };
