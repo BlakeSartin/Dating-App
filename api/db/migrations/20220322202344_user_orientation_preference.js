48d47e42b23d500b14c25846a8316a21ac4c4e36
Thomas Ngo <thomasngo818@gmail.com>
2022-03-22 13:32:38 -0700

    Add down function implementation to drop table

--- api/db/migrations/20220322202344_user_orientation_preference.js
+++ api/db/migrations/20220322202344_user_orientation_preference.js
@@ -20,4 +20,6 @@ exports.up = function (knex) {
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema.dropTable("user_orientation_preference");
+};
