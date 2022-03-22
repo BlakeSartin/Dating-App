f80178d7d3590b41a2a146dc618a0e86b2e1c584
Thomas Ngo <thomasngo818@gmail.com>
2022-03-21 19:11:20 -0700

    Add implementation of up function for creating user gender preference table

--- api/db/migrations/20220322020510_user_gender_preference.js
+++ api/db/migrations/20220322020510_user_gender_preference.js
@@ -2,7 +2,13 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("user_gender_preference", function (table) {
+    table.increments("id");
+    table.integer("user_id").references("id").inTable("users");
+    table.integer("gender_id").references("id").inTable("genders");
+  });
+};
 
 /**
  * @param { import("knex").Knex } knex
