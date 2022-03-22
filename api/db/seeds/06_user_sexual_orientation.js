8af6d8b9f473f07bf6e448ae37175ddeebf3c6cc
Thomas Ngo <thomasngo818@gmail.com>
2022-03-22 10:19:36 -0700

    Add implementation of seed function to add data insert data to user sexual orientation table

--- api/db/seeds/06_user_sexual_orientation.js
+++ api/db/seeds/06_user_sexual_orientation.js
@@ -5,6 +5,19 @@
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("table_name").del();
+
+  // create 5 sexual orientation for each user
+  userOrientations = [];
+  const fakeUserCount = 1000;
+  for (let i = 1; i <= fakeUserCount; i++) {
+    for (let j = 0; j < 5; j++) {
+      userIdentities.push({
+        user_id: i,
+        gender_id: randomPosition(orientationNames),
+      });
+    }
+  }
+
   await knex("table_name").insert([
     { id: 1, colName: "rowValue1" },
     { id: 2, colName: "rowValue2" },
@@ -35,3 +48,8 @@ const orientationNames = [
   "Akioromantic",
   "Aroflux",
 ];
+
+// helper function to randomly select position number of array starting from 1
+const randomPosition = (array) => {
+  return Math.floor(Math.random() * array.length) + 1;
+};
