4f9cd870ebd37d35e032c71396d9c022fbaba1cb
Thomas Ngo <thomasngo818@gmail.com>
2022-03-22 10:08:32 -0700

    Implement up function for creating table and columns

--- api/db/migrations/20220322170110_user_sexual_orientation.js
+++ api/db/migrations/20220322170110_user_sexual_orientation.js
@@ -2,7 +2,16 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function (knex) {};
+exports.up = function (knex) {
+  return knex.schema.createTable("user_sexual_orientation", function (table) {
+    table.increments("id");
+    table.integer("user_id").references("id").inTable("users");
+    table
+      .integer("orientation_id")
+      .references("id")
+      .inTable("sexual_orientations");
+  });
+};
 
 /**
  * @param { import("knex").Knex } knex
