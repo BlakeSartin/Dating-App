94c5a10b53157ed79087650a162b49afc152c2c3
Thomas Ngo <thomasngo818@gmail.com>
2022-03-21 19:41:44 -0700

    Update syntax error with helpers export

--- api/db/seeds/04_user_gender_preference.js
+++ api/db/seeds/04_user_gender_preference.js
@@ -1,4 +1,3 @@
-import { generateGenderId } from "../helpers";
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -6,8 +5,9 @@ import { generateGenderId } from "../helpers";
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("user_gender_preference").del();
-
-  const userPreferences = [];
+  console.log(generateGenderId());
+  let userPreferences = [];
+  const fakeUsersCount = 1000;
   // create 5 gender identities for each user
   for (let i = 1; i <= fakeUsersCount; i++) {
     userPreferences.push({
@@ -31,6 +31,33 @@ exports.seed = async function (knex) {
       gender_id: generateGenderId(),
     });
   }
-
   await knex("user_gender_preference").insert(userPreferences);
+};
+const genderNames = [
+  "Woman",
+  "Man",
+  "Agender",
+  "Androgynous",
+  "Bigender",
+  "Cis Man",
+  "Cis Woman",
+  "Genderfluid",
+  "Genderqueer",
+  "Gender Nonconforming",
+  "Hijra",
+  "Intersex",
+  "Non-binary",
+  "Other gender",
+  "Pangender",
+  "Transfeminine",
+  "Transgender",
+  "Trans Man",
+  "Transmasculine",
+  "Transsexual",
+  "Trans Woman",
+  "Two Spirit",
+];
+// helper function to randomly select id number of a gender identity
+const generateGenderId = () => {
+  return Math.floor(Math.random() * genderNames.length) + 1;
 };
