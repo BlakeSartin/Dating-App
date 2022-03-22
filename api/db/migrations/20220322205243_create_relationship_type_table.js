68a99ba35e9f3affaa8e752860761c99a12f4cfb
Thomas Ngo <thomasngo818@gmail.com>
2022-03-22 13:59:13 -0700

    Implement functions to create table with columns and to drop table

--- api/db/migrations/20220322205243_create_relationship_type_table.js
+++ api/db/migrations/20220322205243_create_relationship_type_table.js
@@ -2,14 +2,15 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("relationship_type", function (table) {
+    table.increments("id");
+    table.string("name").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
