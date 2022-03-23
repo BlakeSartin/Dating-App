7964d59ba8caea215c9f4d7b3498e719902a38c6
Thomas Ngo <thomasngo818@gmail.com>
2022-03-22 19:17:55 -0700

    Add implementation of seed function to create fake user photos data

--- api/db/seeds/10_user_photos.js
+++ api/db/seeds/10_user_photos.js
@@ -1,13 +1,22 @@
+// middleware for creating fake data
+const { faker } = require("@faker-js/faker");
+
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex("user_photos").del();
+
+  // create array of user photo objects
+  const userPhotos = [];
+  const fakeUserCount = 1000;
+  for (let i = 1; i <= fakeUserCount; i++) {
+    userPhotos.push({
+      url: faker.internet.avatar(),
+    });
+  }
+
+  await knex("user_photos").insert(userPhotos);
 };
