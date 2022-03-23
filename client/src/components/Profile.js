bad4ddc294beea52658a0db3d2f19764c57b6526
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-03-22 18:16:09 -0700

    Profile page styling

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,20 +1,26 @@
 import React, { useState } from "react"
+import { Avatar } from '@mui/material';
 
 import ProfileButton from "./ProfileButton"
+import "./profile.scss"
 
 export default function Profile(props) {
 
   return (
-    <main>
+    <main id="profile-main">
       <h1 className="profile-header">Profile</h1>
 
-      <img src="" alt="user profile photo" />
-      <h2 className="user-profile-name">John Doe</h2>
+      <section className="profile-user">
+        <Avatar alt="User profile picture" src="" sx={{ width: 56, height: 56 }}/>
+        <h2 className="user-profile-name">John Doe</h2>
+      </section>
 
-      <ProfileButton name="Placeholder"/>
-      <ProfileButton name="Update Preferences"/>
-      <ProfileButton name="Settings"/>
-      <ProfileButton name="Logout"/>
+      <section className="profile-nav">
+        <ProfileButton name="Placeholder"/>
+        <ProfileButton name="Update Preferences"/>
+        <ProfileButton name="Settings"/>
+        <ProfileButton name="Logout"/>
+      </section>
     </main>
   )
 }
