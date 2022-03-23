72c4d5b94373057c8d7d4fcd13b77d52f0259d54
Thomas Ngo <thomasngo818@gmail.com>
2022-03-23 16:20:20 -0700

    Add route to get a specific gender by id

--- api/routes/genders.js
+++ api/routes/genders.js
@@ -11,5 +11,15 @@ module.exports = (knex) => {
       });
   });
 
+  // get a specific gender based on id
+  router.get("/:id", (request, response) => {
+    knex("genders")
+      .select()
+      .whereRaw("id = ?", [request.params.id])
+      .then((genders) => {
+        response.json(genders);
+      });
+  });
+
   return router;
 };
