d3f97dacbf8daaca4f16f4c9df92168045022583
Thomas Ngo <thomasngo818@gmail.com>
2022-03-23 16:28:15 -0700

    Add route to get specific orientation

--- api/routes/orientations.js
+++ api/routes/orientations.js
@@ -2,5 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (knex) => {
+  // get all sexual orientations
+  router.get("/", (request, response) => {
+    knex("sexual_orientations")
+      .select()
+      .then((orientations) => {
+        response.json(orientations);
+      });
+  });
+
+  // get a specific sexual orientation based on id
+  router.get("/:id", (request, response) => {
+    knex("sexual_orientations")
+      .select()
+      .whereRaw("id = ?", [request.params.id])
+      .then((orientations) => {
+        response.json(orientations);
+      });
+  });
+
   return router;
 };
