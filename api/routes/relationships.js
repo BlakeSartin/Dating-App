c52e7fe8a16b42f61c4350f3a96fdf764ce997be
Thomas Ngo <thomasngo818@gmail.com>
2022-03-23 16:48:29 -0700

    Add routes for getting all relationship type information

--- api/routes/relationships.js
+++ api/routes/relationships.js
@@ -2,5 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (knex) => {
+  // get all relationship types
+  router.get("/", (request, response) => {
+    knex("relationship_types")
+      .select()
+      .then((relationships) => {
+        response.json(relationships);
+      });
+  });
+
   return router;
 };
