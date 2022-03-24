b788d622963f75d2f48f313e0927be9cd7a8a7d2
Thomas Ngo <thomasngo818@gmail.com>
2022-03-23 17:25:27 -0700

    Add route to get all photos

--- api/routes/photos.js
+++ api/routes/photos.js
@@ -2,5 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = (knex) => {
+  // get all photos
+  router.get("/", (request, response) => {
+    knex("user_photos")
+      .select()
+      .then((photos) => {
+        response.json(photos);
+      });
+  });
+
   return router;
 };
