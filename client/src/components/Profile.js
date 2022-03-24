ae3f28e74ac3983ea8f2f0138a344ed965704e63
Thomas Ngo <thomasngo818@gmail.com>
2022-03-24 11:16:03 -0700

    Add user context to profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,23 +1,33 @@
-import React, { useState } from "react"
-import { Avatar } from '@mui/material';
+import React, { useState, useContext } from "react";
+import { Avatar } from "@mui/material";
 
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
 
-import ProfileButton from "./ProfileButton"
-import "./profile.scss"
+import ProfileButton from "./ProfileButton";
+import "./profile.scss";
+
+//importing context
+import { userContext } from "../providers/UserProvider";
 
 export default function Profile(props) {
+  const { user } = useContext(userContext);
 
   return (
     <main id="profile-main">
       <h1 className="profile-header">Profile</h1>
 
       <section className="profile-user">
-        <Avatar alt="User profile picture" src="" sx={{ width: 56, height: 56 }}/>
-        <h2 className="user-profile-name">John Doe</h2>
+        <Avatar
+          alt="User profile picture"
+          src=""
+          sx={{ width: 56, height: 56 }}
+        />
+        <h2 className="user-profile-name">
+          {user.first_name} {user.last_name}
+        </h2>
       </section>
 
       <section className="profile-nav">
@@ -29,5 +39,5 @@ export default function Profile(props) {
         </List>
       </section>
     </main>
-  )
+  );
 }
