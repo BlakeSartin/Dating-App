82c3643dd34611e31dfbd1eda58ae7fcc459393e
Thomas Ngo <thomasngo818@gmail.com>
2022-03-25 04:22:59 -0700

    Add implemenation of migration up and down functions

--- api/db/migrations/20220325110310_create_conversations_table.js
+++ api/db/migrations/20220325110310_create_conversations_table.js
@@ -2,14 +2,18 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("conversations", function (table) {
+    table.increments("id").primary().notNullable();
+    table.integer("user_one").references("id").inTable("users");
+    table.integer("user_two").references("id").inTable("users");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("conversations");
 };
