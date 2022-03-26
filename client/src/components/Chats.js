65b2961ce20b850df7f46b84c27556c7111e6c12
Thomas Ngo <thomasngo818@gmail.com>
2022-03-25 17:07:28 -0700

    Add user context and use effect query to get the conversations to be put in a new state

--- client/src/components/Chats.js
+++ client/src/components/Chats.js
@@ -1,4 +1,5 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useContext, useEffect } from "react";
+import axios from "axios";
 import "./chats.scss";
 import {
   IconButton,
@@ -13,36 +14,49 @@ import {
   Avatar,
 } from "@mui/material";
 
-const db = [
-  {
-    name: "Elliot Page",
-    message:
-      "Standing tall, and athletically built with red skin, Apolónia has a knowing feel about .soft, large brown eyes, a narrow nose, and a pointed chin. short, fine, black hair is features a side part.clothing is revealing and mismatched. A particularly noticeable feature is accent which people usually find annoying.",
-    url: "https://i.insider.com/5fc68f9250e71a00115583f4?width=600&format=jpeg&auto=webp",
-    timestamp: "10 mins ago",
-  },
-  {
-    name: "Lil Nas X",
-    message: "Lorem Ipsum",
-    url: "https://i.scdn.co/image/ab6761610000e5ebab6bd6e450cbc7629a9a2381",
-    timestamp: "10 mins ago",
-  },
-  {
-    name: "Laverne Cox",
-    message: "Lorem Ipsum",
-    url: "https://variety.com/wp-content/uploads/2020/06/laverne-cox-variety-big-ticket-podcast.jpg",
-    timestamp: "10 mins ago",
-  },
-  {
-    name: "Elton John",
-    message: "Lorem Ipsum",
-    url: "https://i.guim.co.uk/img/media/d4e28df5b298b942695a45f31f2d47896e59eb88/0_247_3543_2127/master/3543.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d92405afb743d9e8d705697a1cf105ff",
-    timestamp: "10 mins ago",
-  },
-];
+//importing context
+import { userContext } from "../providers/UserProvider";
+
+// const db = [
+//   {
+//     name: "Elliot Page",
+//     message:
+//       "Standing tall, and athletically built with red skin, Apolónia has a knowing feel about .soft, large brown eyes, a narrow nose, and a pointed chin. short, fine, black hair is features a side part.clothing is revealing and mismatched. A particularly noticeable feature is accent which people usually find annoying.",
+//     url: "https://i.insider.com/5fc68f9250e71a00115583f4?width=600&format=jpeg&auto=webp",
+//     timestamp: "10 mins ago",
+//   },
+//   {
+//     name: "Lil Nas X",
+//     message: "Lorem Ipsum",
+//     url: "https://i.scdn.co/image/ab6761610000e5ebab6bd6e450cbc7629a9a2381",
+//     timestamp: "10 mins ago",
+//   },
+//   {
+//     name: "Laverne Cox",
+//     message: "Lorem Ipsum",
+//     url: "https://variety.com/wp-content/uploads/2020/06/laverne-cox-variety-big-ticket-podcast.jpg",
+//     timestamp: "10 mins ago",
+//   },
+//   {
+//     name: "Elton John",
+//     message: "Lorem Ipsum",
+//     url: "https://i.guim.co.uk/img/media/d4e28df5b298b942695a45f31f2d47896e59eb88/0_247_3543_2127/master/3543.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=d92405afb743d9e8d705697a1cf105ff",
+//     timestamp: "10 mins ago",
+//   },
+// ];
 
 function Chats({ name, message, url }) {
+  const { user } = useContext(userContext);
+  const [db, setDb] = useState([]);
   const [people, setPeople] = useState(db.length - 1);
+
+  useEffect(() => {
+    return axios.get(`/api/chats/user/${user.id}`).then((result) => {
+      // console.log("db query result: ", result.data);
+      setDb(result.data);
+    });
+  }, [user.id]);
+
   const childRefs = useMemo(
     () =>
       Array(db.length)
@@ -70,7 +84,7 @@ function Chats({ name, message, url }) {
           sx={{ width: "100%", bgcolor: "background.paper" }}
         >
           <ListItem alignItems="flex-start">
-            <ListItemButton >
+            <ListItemButton>
               <ListItemAvatar>
                 <Avatar alt={person.name} src={person.url} />
               </ListItemAvatar>
@@ -79,7 +93,8 @@ function Chats({ name, message, url }) {
                 primary={person.name}
                 secondary={
                   <React.Fragment>
-                    <Typography className="message_stamp"
+                    <Typography
+                      className="message_stamp"
                       sx={{ display: "inline" }}
                       component="span"
                       variant="body2"
