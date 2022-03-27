fb9933c5b8df3ff7ef378088a80c89ca5222af5f
Thomas Ngo <thomasngo818@gmail.com>
2022-03-27 14:10:15 -0700

    Checkboxes now change user gender preference state

--- client/src/components/GenPrefListItem.js
+++ client/src/components/GenPrefListItem.js
@@ -1,32 +1,42 @@
 import React, { Fragment, useState, useEffect } from "react";
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 export default function GenPrefListItem({ g, ug }) {
   const [test, setTest] = useState({});
 
   useEffect(() => {
     const temp = {};
-    g.map((a) => (
-      temp[a.id] = ug.includes(a.id) ? true : false
-    ))
-    setTest(temp)
+    g.map((a) => (temp[a.id] = ug.includes(a.id) ? true : false));
+    setTest(temp);
   }, []);
 
   const handleCheck = (event) => {
-    setTest({...test, [event.target.id]: event.target.checked})
-  }
+    setTest({ ...test, [event.target.id]: event.target.checked });
+
+    // modify user's gender preference ids
+    if (event.target.checked) {
+      ug.push(parseInt(event.target.id));
+      console.log(ug);
+    }
+    if (!event.target.checked) {
+      const index = ug.indexOf(parseInt(event.target.id));
+      ug.splice(index, 1);
+      console.log(ug);
+    }
+  };
 
   const genderItems = g.map((i) => (
     <ListItem button key={i.id}>
       <ListItemText primary={i.name} />
-      <input type="checkbox" id={i.id} checked={test[i.id]} onChange={handleCheck}/>
+      <input
+        type="checkbox"
+        id={i.id}
+        checked={test[i.id]}
+        onChange={handleCheck}
+      />
     </ListItem>
   ));
 
-  return (
-    <Fragment>
-      {genderItems}
-    </Fragment>
-  )
+  return <Fragment>{genderItems}</Fragment>;
 }
