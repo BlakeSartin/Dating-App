a7cd151cc81aea1e4a32fdbc958601ede6f765f1
Thomas Ngo <thomasngo818@gmail.com>
2022-03-29 12:55:12 -0700

    Adjust users seeding to use avatar images

--- api/db/seeds/01_users.js
+++ api/db/seeds/01_users.js
@@ -1,20 +1,11 @@
 // middleware for creating fake data
 const { faker } = require("@faker-js/faker");
+const rp = require("request-promise");
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-
-// Helper function to generate fake user
-const createFakeUser = () => ({
-  first_name: faker.name.firstName(),
-  last_name: faker.name.lastName(),
-  email: faker.internet.email(),
-  summary: faker.lorem.lines(2),
-  profile: faker.lorem.paragraphs(),
-  avatar: faker.image.people(),
-});
 
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
@@ -29,4 +20,31 @@ exports.seed = async function (knex) {
 
   // add them to database
   await knex("users").insert(fakeUsers);
+};
+
+// Helper function to generate fake user
+const createFakeUser = () => ({
+  first_name: faker.name.firstName(),
+  last_name: faker.name.lastName(),
+  email: faker.internet.email(),
+  summary: faker.lorem.lines(2),
+  profile: faker.lorem.paragraphs(),
+  avatar: faker.image.avatar(),
+});
+
+// get 1000 user images from randomuser api
+// return array of 1000 random images
+const getRandomUsers = () => {
+  rp("https://randomuser.me/api/?results=1").then((data) => {
+    const userImages = [];
+    parsedData = JSON.parse(data);
+
+    for (const result of parsedData.results) {
+      // console.log(result.picture.large);
+      userImages.push(result.picture.large);
+      console.log(userImages);
+    }
+
+    return userImages;
+  });
 };
