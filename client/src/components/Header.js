a116ed7a33438c4a7c531a02ba7788419ce5cfd2
Blake Sartin <blake.sartin8@gmail.com>
2022-03-29 14:08:01 -0700

    add register styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { IconButton } from "@mui/material";
 import { AccountBox, Chat, VolunteerActivism } from "@mui/icons-material";
-import { AppBar, Toolbar, Box, Typography, Button } from "@mui/material";
+import { AppBar, Toolbar, Box, Typography, Container } from "@mui/material";
+import PropTypes from "prop-types";
 import "./header.scss";
 
-const header = () => {
+const header = (props) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar className="navbar">
-          <Typography sx={{ mr: 2 }}/>
         <Typography
             variant="h6"
             sx={{ flexGrow: 1 }}
@@ -19,37 +19,33 @@ const header = () => {
             style={{ fontSize: 50 }}
             fontFamily="Snell Roundhand, cursive"
           >
-            Q
+            <Container className="logo_header">Q</Container>
           </Typography>
+          <Typography  sx={{ flexGrow: 1 }} />
           <Typography variant="h6" component="div">
-            <IconButton
-              size="large"
-              edge="start"
-              aria-label="menu"
-              
-            >
+            <IconButton size="large" edge="start" aria-label="menu">
               <Link to="/">
-                <VolunteerActivism className="hand_heart" fontSize="large" sx={{ fontSize: 30 }}/>
+                <VolunteerActivism
+                  className="hand_heart"
+                  fontSize="large"
+                  sx={{ fontSize: 30 }}
+                />
               </Link>
             </IconButton>
           </Typography>
           
-          <IconButton
-            size="large"
-            edge="start"
-            aria-label="menu"
-          >
+          <IconButton sx={{ flexGrow: 1 }} size="large" edge="start" aria-label="menu">
             <Link to="/chat">
-              <Chat className="chat_bubble" fontSize="large" sx={{ fontSize: 30 }}/>
+              <Chat
+                className="chat_bubble"
+                fontSize="large"
+                sx={{ fontSize: 30 }}
+              />
             </Link>
           </IconButton>
-          <IconButton
-            size="large"
-            edge="start"
-            aria-label="menu"
-          >
+          <IconButton  size="large" edge="start" aria-label="menu">
             <Link className="profile_cube" to="/profile">
-              <AccountBox fontSize="large" sx={{ fontSize: 30 }}/>
+              <AccountBox fontSize="large" sx={{ fontSize: 30 }} />
             </Link>
           </IconButton>
         </Toolbar>
