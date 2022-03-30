18f2025ce02f0ec3f83c85a775f150c6620c120f
Thomas Ngo <thomasngo818@gmail.com>
2022-03-30 09:28:32 -0700

    Update users seeding to add links to local static photos

--- api/db/seeds/01_users.js
+++ api/db/seeds/01_users.js
@@ -11,12 +11,40 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("users").del();
 
-  // Create 1000 fake users
+  const userImages = [];
   const fakeUsers = [];
   const desiredFakeUsers = 1000;
-  for (let i = 0; i < desiredFakeUsers; i++) {
-    fakeUsers.push(createFakeUser());
-  }
+  // get 1000 profile images
+  await rp("https://randomuser.me/api/?results=1000").then((data) => {
+    parsedData = JSON.parse(data);
+
+    // // add images from api call
+    // for (const result of parsedData.results) {
+    //   userImages.push(result.picture.large);
+    //   console.log(userImages);
+    // }
+
+    // add images saved locally
+    for (let i = 0; i <= desiredFakeUsers; i++) {
+      userImages.push(
+        `/photos/0${Math.floor(Math.random() * 10)}${Math.floor(
+          Math.random() * 10
+        )}.avif`
+      );
+    }
+
+    // Create 1000 fake users
+    for (let i = 0; i < desiredFakeUsers; i++) {
+      fakeUsers.push({
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        email: faker.internet.email(),
+        summary: faker.lorem.lines(2),
+        profile: faker.lorem.lines(2),
+        avatar: userImages[i],
+      });
+    }
+  });
 
   // add them to database
   await knex("users").insert(fakeUsers);
@@ -35,7 +63,7 @@ const createFakeUser = () => ({
 // get 1000 user images from randomuser api
 // return array of 1000 random images
 const getRandomUsers = () => {
-  rp("https://randomuser.me/api/?results=1").then((data) => {
+  rp("https://randomuser.me/api/?results=1000").then((data) => {
     const userImages = [];
     parsedData = JSON.parse(data);
 
