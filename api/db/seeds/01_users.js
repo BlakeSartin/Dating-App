89dbb09423d1f969d6bf953d9dc8cfd26cba8f23
Thomas Ngo <thomasngo818@gmail.com>
2022-03-30 10:59:53 -0700

    Add pronouns to summary

--- api/db/seeds/01_users.js
+++ api/db/seeds/01_users.js
@@ -29,7 +29,9 @@ exports.seed = async function (knex) {
       first_name: faker.name.firstName(),
       last_name: faker.name.lastName(),
       email: faker.internet.email(),
-      summary: faker.lorem.lines(2),
+      summary: `Pronouns: ${
+        pronouns[Math.floor(Math.random() * pronouns.length)]
+      }`,
       profile: faker.lorem.lines(2),
       avatar: userImages[i],
     });
@@ -39,29 +41,5 @@ exports.seed = async function (knex) {
   await knex("users").insert(fakeUsers);
 };
 
-// Helper function to generate fake user
-const createFakeUser = () => ({
-  first_name: faker.name.firstName(),
-  last_name: faker.name.lastName(),
-  email: faker.internet.email(),
-  summary: faker.lorem.lines(2),
-  profile: faker.lorem.paragraphs(),
-  avatar: faker.image.avatar(),
-});
-
-// get 1000 user images from randomuser api
-// return array of 1000 random images
-const getRandomUsers = () => {
-  rp("https://randomuser.me/api/?results=1000").then((data) => {
-    const userImages = [];
-    parsedData = JSON.parse(data);
-
-    for (const result of parsedData.results) {
-      // console.log(result.picture.large);
-      userImages.push(result.picture.large);
-      console.log(userImages);
-    }
-
-    return userImages;
-  });
-};
+// array of pronoun strings
+const pronouns = ["He/Him", "She/Her", "They/Them"];
