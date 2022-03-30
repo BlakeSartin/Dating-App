0e4f306b014e6898376d7df7628d0e92a8f94414
Thomas Ngo <thomasngo818@gmail.com>
2022-03-30 09:56:01 -0700

    Update summary to include gender identity and sexual orientation information

--- client/src/components/CardBackdrop.js
+++ client/src/components/CardBackdrop.js
@@ -3,7 +3,11 @@ import { IconButton, Backdrop, Button } from "@mui/material";
 import { ArrowDropDownCircle } from "@mui/icons-material";
 import "./card_backdrop.scss";
 
-export default function CardBackdrop({ summary, name }) {
+export default function CardBackdrop({
+  summary,
+  genderIdentity,
+  sexualOrientation,
+}) {
   const [open, setOpen] = React.useState(false);
   const handleClose = () => {
     setOpen(false);
@@ -15,13 +19,27 @@ export default function CardBackdrop({ summary, name }) {
   return (
     <div>
       <IconButton onClick={handleToggle}>
-        <ArrowDropDownCircle className="arrow_circle" fontSize="large" sx={{ fontSize: 30 }} />
+        <ArrowDropDownCircle
+          className="arrow_circle"
+          fontSize="large"
+          sx={{ fontSize: 30 }}
+        />
       </IconButton>
       <Backdrop
-        sx={{ color: "aquamarine", zIndex: (theme) => theme.zIndex.drawer + 1}}
+        sx={{ color: "aquamarine", zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={open}
         onClick={handleClose}
       >
+        <h4 className="backdrop_text">
+          {genderIdentity.map((e) => {
+            return " " + e + " |";
+          })}
+        </h4>
+        <h4 className="backdrop_text">
+          {sexualOrientation.map((e) => {
+            return " " + e + " |";
+          })}
+        </h4>
         <h4 className="backdrop_text">{summary}</h4>
       </Backdrop>
     </div>
