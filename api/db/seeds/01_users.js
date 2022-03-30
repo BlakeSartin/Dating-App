3fc6800593467b2aa106ecf4b6f1cab0a4ae5509
Thomas Ngo <thomasngo818@gmail.com>
2022-03-30 10:30:12 -0700

    Remove unused api call

--- api/db/seeds/01_users.js
+++ api/db/seeds/01_users.js
@@ -1,6 +1,5 @@
 // middleware for creating fake data
 const { faker } = require("@faker-js/faker");
-const rp = require("request-promise");
 
 /**
  * @param { import("knex").Knex } knex
@@ -14,37 +13,27 @@ exports.seed = async function (knex) {
   const userImages = [];
   const fakeUsers = [];
   const desiredFakeUsers = 1000;
-  // get 1000 profile images
-  await rp("https://randomuser.me/api/?results=1000").then((data) => {
-    parsedData = JSON.parse(data);
 
-    // // add images from api call
-    // for (const result of parsedData.results) {
-    //   userImages.push(result.picture.large);
-    //   console.log(userImages);
-    // }
+  // add images saved locally
+  for (let i = 0; i <= desiredFakeUsers; i++) {
+    userImages.push(
+      `/photos/0${Math.floor(Math.random() * 10)}${Math.floor(
+        Math.random() * 10
+      )}.avif`
+    );
+  }
 
-    // add images saved locally
-    for (let i = 0; i <= desiredFakeUsers; i++) {
-      userImages.push(
-        `/photos/0${Math.floor(Math.random() * 10)}${Math.floor(
-          Math.random() * 10
-        )}.avif`
-      );
-    }
-
-    // Create 1000 fake users
-    for (let i = 0; i < desiredFakeUsers; i++) {
-      fakeUsers.push({
-        first_name: faker.name.firstName(),
-        last_name: faker.name.lastName(),
-        email: faker.internet.email(),
-        summary: faker.lorem.lines(2),
-        profile: faker.lorem.lines(2),
-        avatar: userImages[i],
-      });
-    }
-  });
+  // Create 1000 fake users
+  for (let i = 0; i < desiredFakeUsers; i++) {
+    fakeUsers.push({
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+      summary: faker.lorem.lines(2),
+      profile: faker.lorem.lines(2),
+      avatar: userImages[i],
+    });
+  }
 
   // add them to database
   await knex("users").insert(fakeUsers);
